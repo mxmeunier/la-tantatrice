10da2860ab6b376932ed19160e3c9f764b576d87
maxime-meunier <maxime.meunier@forcity.com>
2019-04-23 11:50:39 +0200

    core: add webpack & vue-loader

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+'use strict';
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};    
